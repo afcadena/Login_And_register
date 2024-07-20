024dee489be6ffd8308a17ae4eb90419ce86dfb6
afcadena <afcadena022@gmail.com>
2024-07-19 21:32:28 -0500

    Avance Proyecto

--- app/src/main/java/co/edu/sena/login_and_register/componentes/AppComponents.kt
+++ app/src/main/java/co/edu/sena/login_and_register/componentes/AppComponents.kt
@@ -1,26 +1,40 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package co.edu.sena.login_and_register.componentes
+
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.AutofillType.Password
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.edu.sena.login_and_register.R
 
 
+private val Any.VisibilityOff: Unit
+    get() {}
+private val Any.Visibility: Unit
+    get() {}
 
 @Composable
 fun NormalTextComponent(value:String){
@@ -63,21 +77,87 @@ fun HeadingTextComponent(value:String){
     )
 }
 
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTextField(labelValue:String){
+fun MyTextField(labelValue: String, icon: @Composable () -> Unit) {
 
     val textValue = remember {
-        mutableStateOf(value = )
+        mutableStateOf("")
     }
+
     OutlinedTextField(
-        label = Text(text = stringResource(id = )),
-        value = textValue ,
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text(text = labelValue) },
+        value = textValue.value,
         colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = colorResource(id = R.color.teal_200)
-            focusedBorderColor
+            focusedBorderColor = colorResource(id = R.color.teal_200),
+            focusedLabelColor = colorResource(id = R.color.teal_200),
+            cursorColor = colorResource(id = R.color.teal_200),
+
+
         ),
-        onValueChange ={
-        textValue.value = it
-    } )
+        onValueChange = {
+            textValue.value = it
+        },
+        leadingIcon = icon
+    )
+}
+
+fun Icon(painter: Unit, contentDescription: String) {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordTextField(labelValue: String, icon: @Composable () -> Unit) {
+
+    val password = remember {
+        mutableStateOf("")
+    }
+
+    val passwordVisible = remember{
+        mutableStateOf(false)
+    }
+
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        label = { Text(text = labelValue) },
+        value = password.value,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = colorResource(id = R.color.teal_200),
+            focusedLabelColor = colorResource(id = R.color.teal_200),
+            cursorColor = colorResource(id = R.color.teal_200),
+
+
+            ), keyboardOptions = keyboardOptions(keyboardType = Password),
+        onValueChange = {
+            password.value = it
+        },
+        leadingIcon = icon,
+        trailingIcon = {
+            val iconImage = if(passwordVisible.value){
+                Icons.Filled.Visibility
+            }else{
+                Icons.Filled.VisibilityOff
+            }
+
+            val description = if(passwordVisible.value){
+                stringResource(id = R.string.hide_password)
+            }else{
+                stringResource(id = R.string.show_password)
+            }
+            IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
+                Icon(painter = iconImage, contentDescription = description)
+            }
+
+        },
+        visualTransformation = if(passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation()
+    )
+}
+
+fun keyboardOptions(keyboardType: Any): KeyboardOptions {
+    TODO("Not yet implemented")
 }
--- app/src/main/java/co/edu/sena/login_and_register/paginas/PaginaInicioSesion.kt
+++ app/src/main/java/co/edu/sena/login_and_register/paginas/PaginaInicioSesion.kt
@@ -2,8 +2,15 @@ package co.edu.sena.login_and_register.paginas
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Lock
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,13 +20,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import co.edu.sena.login_and_register.R
 import co.edu.sena.login_and_register.componentes.HeadingTextComponent
+import co.edu.sena.login_and_register.componentes.MyTextField
 import co.edu.sena.login_and_register.componentes.NormalTextComponent
+import co.edu.sena.login_and_register.componentes.PasswordTextField
 
 
 @Composable
 fun PaginaInicioSesion() {
-
-    Surface(color = Color.White,
+    Surface(
+        color = Color.White,
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
@@ -28,11 +37,50 @@ fun PaginaInicioSesion() {
         Column(modifier = Modifier.fillMaxSize()) {
             NormalTextComponent(value = stringResource(id = R.string.Hello))
             HeadingTextComponent(value = stringResource(id = R.string.create))
+            Spacer(modifier = Modifier.height(20.dp))
+
+            MyTextField(
+                labelValue = stringResource(id = R.string.user),
+                icon = {
+                    Icon(
+                        Icons.Rounded.Person,
+                        contentDescription = null
+                    )
+                }
+            )
+
+            MyTextField(
+                labelValue = stringResource(id = R.string.user_l),
+                icon = {
+                    Icon(
+                        Icons.Rounded.Person,
+                        contentDescription = null
+                    )
+                }
+            )
+
+            MyTextField(
+                labelValue = stringResource(id = R.string.email),
+                icon = {
+                    Icon(
+                        Icons.Rounded.Email,
+                        contentDescription = null
+                    )
+                }
+            )
+            PasswordTextField(
+                labelValue = stringResource(id = R.string.password),
+                icon = {
+                    Icon(
+                        Icons.Rounded.Lock,
+                        contentDescription = null
+                    )
+                }
+            )
         }
-
     }
-
 }
+
 
 @Preview
 @Composable
